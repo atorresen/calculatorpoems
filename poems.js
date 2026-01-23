139843e438d75a7a8636591fc216d83291ce27b7
atorresen <torresen@umich.edu>
2026-01-22 20:31:52 -0500

    Add Less Oboe poem, create file paths programmatically

--- poems.js
+++ poems.js
@@ -1,30 +1,33 @@
 const poems = [
-	"his legless bliss is legible\nshoeless he sighs\nless he hobbles\nhe seizes glee\nblesses ill legs\nheels his sole",
-	"goo oozes loose\nbile sloshes\nboils gloss\ngel soils booboos\nlobes loll\noh gosh",
-	"she sobs\nshe sighs\nsobs ebb\nshe giggles"
-];
-
-const poemfiles = [
 	[
-		["images/Legless_Bliss_1_numbers.png", "images/Legless_Bliss_1_letters.png"],
-		["images/Legless_Bliss_2_numbers.png", "images/Legless_Bliss_2_letters.png"],
-		["images/Legless_Bliss_3_numbers.png", "images/Legless_Bliss_3_letters.png"],
-		["images/Legless_Bliss_4_numbers.png", "images/Legless_Bliss_4_letters.png"],
-		["images/Legless_Bliss_5_numbers.png", "images/Legless_Bliss_5_letters.png"],
-		["images/Legless_Bliss_6_numbers.png", "images/Legless_Bliss_6_letters.png"]
+		"Legless_Bliss",
+		"his legless bliss is legible\nshoeless he sighs\nless he hobbles\nhe seizes glee\nblesses ill legs\nheels his sole"
 	],
 	[
-		["images/Plague_1_numbers.png", "images/Plague_1_letters.png"],
-		["images/Plague_2_numbers.png", "images/Plague_2_letters.png"],
-		["images/Plague_3_numbers.png", "images/Plague_3_letters.png"],
-		["images/Plague_4_numbers.png", "images/Plague_4_letters.png"],
-		["images/Plague_5_numbers.png", "images/Plague_5_letters.png"],
-		["images/Plague_6_numbers.png", "images/Plague_6_letters.png"]
+		"Plague",
+		"goo oozes loose\nbile sloshes\nboils gloss\ngel soils booboos\nlobes loll\noh gosh"
 	],
 	[
-		["images/She_Sobs_1_numbers.png", "images/She_Sobs_1_letters.png"],
-		["images/She_Sobs_2_numbers.png", "images/She_Sobs_2_letters.png"],
-		["images/She_Sobs_3_numbers.png", "images/She_Sobs_3_letters.png"],
-		["images/She_Sobs_4_numbers.png", "images/She_Sobs_4_letters.png"]
+		"She_Sobs",
+		"she sobs\nshe sighs\nsobs ebb\nshe giggles"
+	],
+	[
+		"Less_Oboe",
+		"shhhhhh\nshe hisses\nbegs bliss\nobliges less oboe"
 	]
 ];
+
+let fileNames = [];
+
+poems.forEach(info => {
+	let poemFiles = [];
+	let title = info[0];
+	let numLines = info[1].split("\n").length + 1;
+	for (let i = 1; i < numLines; i++) {
+		poemFiles.push([
+			"images/" + title + "_" + i + "_numbers.png",
+			"images/" + title + "_" + i + "_letters.png"
+		])
+	};
+	fileNames.push(poemFiles)
+});
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 var pos = 0;
 const poemtable = document.getElementById("poem");
-// poems and poemfiles are defined in a separate .js file to keep this script.js file tidy
-const len = poemfiles.length;
+// poems and fileNames are defined in a separate .js file to keep this script.js file tidy
+const len = fileNames.length;
 var flipped = [];
-for (p of poemfiles) {
+for (p of fileNames) {
 	flipped.push(Array.from("1".repeat(p.length)))
 }
 
 function slide(n) {
 	pos = ((pos + n % len) + len) % len;
+	textQ = false;
+	document.getElementById("text").innerText = "";
 	show(pos)
 }
 
@@ -17,7 +19,7 @@ function showtext() {
 	textQ = !textQ;
 	const text = document.getElementById("text");
 	if (textQ) {
-		text.innerText = poems[pos]
+		text.innerText = poems[pos][1]
 	} else {
 		text.innerText = ""
 	}
@@ -27,7 +29,7 @@ function show(n) {
 	while (poemtable.firstChild) {
 		poemtable.removeChild(poemtable.firstChild);
 	};
-	const poemlines = poemfiles[n];
+	const poemlines = fileNames[n];
 	for (i = 0; i < poemlines.length; i++) {
 		const row = poemtable.insertRow();
 		const button = row.insertCell();
@@ -47,7 +49,7 @@ function flipButton(n, i) {
 	button.onclick = function() {
 		let f = flipped[n][i];
 		const line = document.getElementById("line" + n + i);
-		line.src = poemfiles[n][i][f];
+		line.src = fileNames[n][i][f];
 		flipped[n][i] = (f + 1) % 2;
 	}
 	return button
